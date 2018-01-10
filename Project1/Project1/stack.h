c9e1ec4fc9728c3f0c182c851b41006ab6793a59
Damon Z Xie <dxie015@nam.ad.pwcinternal.com>
2018-01-10 14:31:48 +0800

    01 10

--- Project1/Project1/stack.h
+++ Project1/Project1/stack.h
@@ -1,8 +1,19 @@
 #ifndef STACK_H
 #define STACK_H
 
+#include <stdlib.h>
+#include "list.h"
 
+typedef List Stack;
 
+#define stack_init list_init
+#define stack_destroy list_destroy
+
+#define stack_peek(stack) ((stack)->head == NULL ? NULL : (stack)->head->data)
+#define stack_size list_size
+
+int stack_push(Stack *stack, const void *data);
+int stack_pop(Stack *stack, void **data);
 
 #endif // !STACK_H
 
